f17c844a2600c9ec867b18ce88d4f127e96d1242
Sec <qianqian1903@gmail.com>
2022-08-07 23:25:34 +0800

    changed some js logics

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,14 +1,39 @@
 const btn = document.querySelector("#mobile-menu-btn");
 const closeBtn = document.querySelector("#close-btn");
 const dialog = document.querySelector("#dialog");
+const mobileMenuList = document.querySelector("#mobile-menu-list");
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", (e) => {
     // document.body.classList.add("overflow");
-    dialog.classList.add("show");
+    e.stopPropagation();
     dialog.showModal();
+    mobileMenuList.classList.add("show");
 });
 
-closeBtn.addEventListener("click", () => {
-    dialog.close();
-    dialog.classList.remove("show");
-});
+const closeDialog = (e) => {
+    if (e.target.closest("#close-btn") || e.target.id === "dialog") {
+        dialog.close();
+        dialog.classList.remove("show");
+    }
+};
+
+closeBtn.addEventListener("click", closeDialog);
+
+const config = { attributes: true };
+const callback = (mutations) => {
+    console.log(mutations);
+    for (const mutation of mutations) {
+        const { target } = mutation;
+        if (!target.open) {
+            document.removeEventListener("click", closeDialog);
+            return;
+        }
+        document.addEventListener("click", closeDialog);
+        // console.log(target.getAttributes("open"));
+    }
+    // const { target } = mutations;
+    // console.log(target);
+    // if (target.classList.includes("show") && target.getAttributes("show"))
+};
+const observer = new MutationObserver(callback);
+observer.observe(dialog, config);
